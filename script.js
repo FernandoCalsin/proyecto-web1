a0ebb806ace658e224a5e48b1812614cc4567915
fernando <139663114+FernandoCalsin@users.noreply.github.com>
2025-10-14 00:03:15 -0500

    agregando el logo y agregnado a las pestañas deslisantes de Suscribete contactanos contenido

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function abrirMenu() {
-    document.getElementById("menu-lateral").style.width = "100%";
+// Función genérica para abrir CUALQUIER overlay por su ID
+function abrirOverlay(overlayId) {
+  document.getElementById(overlayId).style.width = "100%";
 }
 
-function cerrarMenu() {
-    document.getElementById("menu-lateral").style.width = "0";
+// Función genérica para cerrar CUALQUIER overlay por su ID
+function cerrarOverlay(overlayId) {
+  document.getElementById(overlayId).style.width = "0";
 }
